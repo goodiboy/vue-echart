d9e246e7125ed040b63fed5bf2a10d3346b344ff
阮传宙 <ruanchuanzhou@interlib.vip>
2021-03-14 15:19:56 +0800

    调整请求头允许跨域

--- back-code/middleware/koa_response_header.ts
+++ back-code/middleware/koa_response_header.ts
@@ -8,5 +8,9 @@ export default async (
 ) => {
   const contentType = 'application/json;charset=utf-8'
   ctx.set('Content-Type', contentType)
+  ctx.set('Access-Control-Allow-Origin', '*') //允许来自所有域名请求(不携带cookie请求可以用*，如果有携带cookie请求必须指定域名)
+  // ctx.set("Access-Control-Allow-Origin", "http://localhost:8080"); // 只允许指定域名http://localhost:8080的请求
+
+  ctx.set('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, POST, DELETE')
   await next()
 }
--- back-code/utils/file_utils.ts
+++ back-code/utils/file_utils.ts
@@ -1,11 +1,15 @@
 import * as fs from 'fs'
 
+/**
+ * 读取文件数据
+ * @param filePath => 文件的路径
+ * @return string => 字符串文件数据
+ */
 export const getFileJsonData = (filePath: string): string => {
   let fileData: string
   try {
     fileData = fs.readFileSync(filePath, 'utf-8')
   } catch (e) {
-    console.log(e)
     const errMsg = {
       message: '读取文件失败，文件资源不存在',
       code: 404
