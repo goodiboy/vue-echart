25cdab0d32b0ab12ae953221c3544d3bc796688b
阮传宙 <ruanchuanzhou@interlib.vip>
2021-03-17 22:55:16 +0800

    完成商家销售统计图表（柱状图表）

--- front-code/src/components/Seller.vue
+++ front-code/src/components/Seller.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class='com-container'>
+  <div class="com-container">
     <div
-      @mouseenter='endInterval'
-      @mouseleave='startInterval'
-      ref='seller'
-      class='com-chart'
+      @mouseenter="endInterval"
+      @mouseleave="startInterval"
+      ref="seller"
+      class="com-chart"
     ></div>
   </div>
 </template>
 
-<script lang='ts'>
+<script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 import { dataType } from '@/types/types'
 // 引入主题
@@ -31,14 +31,49 @@ export default class Seller extends Vue {
   private UPDATE_TIME = 3000
   // 定时器对象
   private intervalTimerId = 0
+  // echarts挂载的DOM节点
+  private echartsDOM: HTMLElement = {} as HTMLElement
 
-  mounted() {
+  mounted(): void {
     this.initChart()
     this.getData()
+    this.resizeRender()
+    window.addEventListener('resize', this.resizeRender)
   }
 
-  destroy() {
+  destroy(): void {
+    // 组件销毁的时候，清除定时器
     this.endInterval()
+    // 组件销毁的时候，清除事件
+    window.removeEventListener('resize', this.resizeRender)
+  }
+
+  // 浏览器大小发生变化时触发
+  resizeRender(): void {
+    const titleFontSize = (this.echartsDOM.offsetWidth / 100) * 3.6
+    // 分辨率大小相关配置
+    const option = {
+      title: {
+        textStyle: {
+          fontSize: titleFontSize
+        }
+      },
+      tooltip: {
+        axisPointer: {
+          lineStyle: {
+            width: titleFontSize
+          }
+        }
+      },
+      series: {
+        barWidth: titleFontSize,
+        itemStyle: {
+          borderRadius: [0, titleFontSize / 2, titleFontSize / 2, 0]
+        }
+      }
+    }
+    this.chartInstance.setOption(option)
+    this.chartInstance.resize()
   }
 
   // 获取接口数据
@@ -56,10 +91,86 @@ export default class Seller extends Vue {
 
   // 初始化echarts实例
   private initChart(): void {
-    this.chartInstance = this.$echarts.init(
-      this.$refs.seller as HTMLElement,
-      'chalk'
-    )
+    this.echartsDOM = this.$refs.seller as HTMLElement
+    this.chartInstance = this.$echarts.init(this.echartsDOM, 'chalk')
+    this.initCharData()
+  }
+
+  // 初始化echarts的选项配置
+  initCharData(): void {
+    const { names, values } = this.getShowData()
+    const option = {
+      title: {
+        text: '▎商家销售统计',
+        left: 40,
+        top: 20
+      },
+      grid: {
+        top: '15%',
+        left: '3%',
+        right: '6%',
+        bottom: '3%',
+        containLabel: true // 距离是包括坐标轴上的位置
+      },
+      xAxis: {
+        type: 'value'
+      },
+      yAxis: {
+        type: 'category',
+        data: names
+      },
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'line',
+          z: 0,
+          lineStyle: {
+            type: 'solid',
+            color: '#2d3443'
+          }
+        }
+      },
+      series: {
+        type: 'bar',
+        data: values,
+        label: {
+          show: true,
+          position: 'right',
+          color: 'white'
+        },
+        itemStyle: {
+          /*
+          // 方法一
+          color: {
+            type: 'linear',
+            x:0,
+            y:0,
+            x2:1,
+            y2:0,
+            colorStops: [
+              {
+                offset: 0, color: '#5053ee'
+              },
+              {
+                offset: 1, color: '#ab6ee5'
+              }
+            ]
+          }
+          */
+          color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
+            {
+              offset: 0,
+              color: '#5053ee'
+            },
+            {
+              offset: 1,
+              color: '#ab6ee5'
+            }
+          ])
+        }
+      }
+    }
+    this.chartInstance.setOption(option)
   }
 
   // 定时更新数据
@@ -95,85 +206,18 @@ export default class Seller extends Vue {
     return { values, names }
   }
 
-  // 更新echarts视图
+  // 更新echarts数据
   updateChart(): void {
     const { names, values } = this.getShowData()
     const option = {
-      title: {
-        text: '▎商家销售统计',
-        textStyle: {
-          fontSize: 66
-        },
-        left: 40,
-        top: 20
-      },
-      grid: {
-        top: '15%',
-        left: '3%',
-        right: '6%',
-        bottom: '3%',
-        containLabel: true // 距离是包括坐标轴上的位置
-      },
-      xAxis: {
-        type: 'value'
-      },
       yAxis: {
-        type: 'category',
         data: names
       },
-      tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-          type: 'line',
-          z: 0,
-          lineStyle: {
-            type: 'solid',
-            width: 66,
-            color: '#2d3443'
-          }
-        }
-      },
       series: {
-        type: 'bar',
-        data: values,
-        barWidth: 66,
-        label: {
-          show: true,
-          position: 'right',
-          color: 'white'
-        },
-        itemStyle: {
-          borderRadius: [0, 33, 33, 0],
-          /*
-          // 方法一
-          color: {
-            type: 'linear',
-            x:0,
-            y:0,
-            x2:1,
-            y2:0,
-            colorStops: [
-              {
-                offset: 0, color: '#5053ee'
-              },
-              {
-                offset: 1, color: '#ab6ee5'
-              }
-            ]
-          }
-          */
-          color: new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [
-            {
-              offset: 0, color: '#5053ee'
-            },
-            {
-              offset: 1, color: '#ab6ee5'
-            }
-          ])
-        }
+        data: values
       }
     }
     this.chartInstance.setOption(option)
   }
-} 
+}
 </script>
